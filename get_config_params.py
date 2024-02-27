bc11668ba86e11bdbff5beafe056f740df8b242b
Maria <mariarosekesa@gmail.com>
2024-02-27 19:30:33 +0200

    cache errors

--- get_config_params.py
+++ get_config_params.py
@@ -1,11 +1,13 @@
 import json
+from exceptions import CachePathNotSpecifiedError
 
 def get_cache_paths(): 
     # Opening JSON file
     f = open('/home/maria/HuggingMouse/config.json')
-    
-    # returns JSON object as 
-    # a dictionary
     config = json.load(f)
 
+    if not config['allen_cache_path']:
+        CachePathNotSpecifiedError("No Allen data cache path specified in config.json!")
+    #Different logic for two caches-- project_cache is optional, but there's no way around
+    #Allen cache
     return config['allen_cache_path'], config.get('project_cache_path', None)
