4dcb0a2a6d1a93034ef95bebefa3695abede299b
Maria <mariarosekesa@gmail.com>
2024-02-27 21:11:01 +0200

    Allen bug fix

--- analyzer.py
+++ analyzer.py
@@ -18,6 +18,10 @@ class MakeEmbeddings:
     raw_data_dct = {}
     movie_one_dataset = boc.get_ophys_experiment_data(session_A)
     raw_data_dct['movie_one'] = movie_one_dataset.get_stimulus_template('natural_movie_one')
+    movie_two_dataset = boc.get_ophys_experiment_data(session_C)
+    raw_data_dct['movie_two'] = movie_two_dataset.get_stimulus_template('natural_movie_two')
+    movie_three_dataset = boc.get_ophys_experiment_data(session_A)
+    raw_data_dct['movie_three'] = movie_three_dataset.get_stimulus_template('natural_movie_three')
 
     def __init__(self, processor, model, cache_data=True):
         self.processor = processor
@@ -27,12 +31,12 @@ class MakeEmbeddings:
 
     def process_stims(self, stims):
         n_stims = len(stims)
-        n_stims=10
-        stims = np.repeat(stims[:, np.newaxis, :, :], 3, axis=1)
+        #n_stims=10
+        stims_dim = np.repeat(stims[:, np.newaxis, :, :], 3, axis=1)
         embeddings = np.empty((n_stims, 768))
         for i in range(n_stims):
             print(i)
-            inputs = self.processor(images=stims[i], return_tensors="pt")
+            inputs = self.processor(images=stims_dim[i], return_tensors="pt")
             with torch.no_grad():
                 outputs = self.model(**inputs)
             cls = outputs.pooler_output.squeeze().detach().numpy()
@@ -44,6 +48,7 @@ class MakeEmbeddings:
         model_string=self.model.name_or_path.replace('/', '_') 
         file_name = model_string+'_embeddings.pkl'
         # Pickle the dictionary
+        save_path=Path(self.project_cache_path) / Path(file_name)
         with open(file_name, 'wb') as f:
             pickle.dump(embeddings_dct, f)
 
@@ -63,7 +68,6 @@ class MakeEmbeddings:
 
 
 if __name__=="__main__":
-    CACHE_PATH='/media/maria/DATA/AllenData'
     #Initialize model and processor
     processor = ViTImageProcessor.from_pretrained('google/vit-base-patch32-384')
     model = ViTModel.from_pretrained('google/vit-base-patch32-384')
