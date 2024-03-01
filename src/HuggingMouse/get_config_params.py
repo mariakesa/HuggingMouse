0f6d523b14131a96d3cf6200d66801c76501fb43
Maria <mariarosekesa@gmail.com>
2024-03-01 03:06:45 +0200

    Man sphinx really is demanding

--- src/HuggingMouse/get_config_params.py
+++ src/HuggingMouse/get_config_params.py
@@ -1,8 +1,20 @@
 import json
 from exceptions import CachePathNotSpecifiedError
 import os
+from pathlib import Path
+
+def create_config_file():
+    config = {
+        "allen_cache_path": str(Path(os.path.dirname(__file__))/Path('AllenCache')),
+        "transformer_embedding_cache_path": str(Path(os.path.dirname(__file__))/Path('TransformerEmbeddings'))
+    }
+    config_path = os.path.join(os.path.dirname(__file__), 'config.json')
+    with open(config_path, 'w') as f:
+        json.dump(config, f, indent=4)
 
 def get_cache_paths(): 
+    if not os.path.exists(os.path.join(os.path.dirname(__file__), 'config.json')):
+        create_config_file()
     # Opening JSON 
     config_path = os.path.join(os.path.dirname(__file__), 'config.json')
     f = open(config_path)
