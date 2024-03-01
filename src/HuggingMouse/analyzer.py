7a13d0ae8988d81a41c3f628adb693fa4e88727e
Maria <mariarosekesa@gmail.com>
2024-03-01 03:27:45 +0200

    still trying

--- src/HuggingMouse/analyzer.py
+++ src/HuggingMouse/analyzer.py
@@ -2,11 +2,15 @@ from transformers import ViTImageProcessor, ViTModel
 from regressors import VisionEmbeddingToNeuronsRegressor
 from sklearn.linear_model import Ridge
 from allen_api_utilities import AllenExperimentUtility
+import os
 
 if __name__=="__main__":
-    regression_model=Ridge(10)
-    model = ViTModel.from_pretrained('google/vit-base-patch32-384')
-    exps=AllenExperimentUtility()
-    exps.view_all_imaged_areas()
-    id=exps.experiment_container_ids_imaged_areas(['VISal'])[0]
-    VisionEmbeddingToNeuronsRegressor(model,regression_model).execute(id)
+    # Check if the code is being executed in a documentation build environment
+    if os.environ.get('READTHEDOCS') != 'True':
+        # Skip API call in documentation build environment
+        regression_model=Ridge(10)
+        model = ViTModel.from_pretrained('google/vit-base-patch32-384')
+        exps=AllenExperimentUtility()
+        exps.view_all_imaged_areas()
+        id=exps.experiment_container_ids_imaged_areas(['VISal'])[0]
+        VisionEmbeddingToNeuronsRegressor(model,regression_model).execute(id)
