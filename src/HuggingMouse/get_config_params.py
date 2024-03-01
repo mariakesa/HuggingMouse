faee3ca3c9528598d2fc5d5ce2cb0d630feefacc
Maria <mariarosekesa@gmail.com>
2024-03-01 02:55:33 +0200

    fix

--- src/HuggingMouse/get_config_params.py
+++ src/HuggingMouse/get_config_params.py
@@ -1,9 +1,11 @@
 import json
 from exceptions import CachePathNotSpecifiedError
+import os
 
 def get_cache_paths(): 
-    # Opening JSON file
-    f = open('config.json')
+    # Opening JSON 
+    config_path = os.path.join(os.path.dirname(__file__), 'config.json')
+    f = open(config_path)
     config = json.load(f)
 
     if not config['allen_cache_path']:
