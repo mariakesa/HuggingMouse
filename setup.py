6c5e9dfc98d0800f065cdb9fc0baca3bb25f88e2
Maria <mariarosekesa@gmail.com>
2024-03-22 00:28:13 +0200

    setup py and pyproject

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+setup(name='HuggingMouse',
+      version='0.0.1',
+      description='Data analysis library for Allen Brain Observatory data',
+      author='Maria Kesa',
+      author_email='mariarosekesa@gmail.com',
+      url='https://github.com/mariakesa/HuggingMouse',
+      install_requires=[
+          'scikit-learn==1.2.2',
+          'torch==1.13.1',
+          'pandas==1.5.3',
+          'numpy==1.23.5',
+          'transformers==4.31.0',
+          'allensdk==2.16.2'
+      ],
+      packages=find_packages("src"),
+      package_dir={"": "src"}
+      )
