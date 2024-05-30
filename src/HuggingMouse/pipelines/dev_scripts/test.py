8feeb6e14e77c6d871b17c071ae7d084cca28d19
Maria <mariarosekesa@gmail.com>
2024-05-30 15:54:13 +0300

    pipeline

--- src/HuggingMouse/pipelines/dev_scripts/test.py
+++ src/HuggingMouse/pipelines/dev_scripts/test.py
@@ -7,13 +7,18 @@ from pipelines import pipeline
 from dotenv import load_dotenv
 from single_trial_fs import MovieSingleTrialRegressionAnalysis
 from transformers import ViTModel
+from sklearn.linear_model import LinearRegression
 
 load_dotenv()
 
 model = ViTModel.from_pretrained('google/vit-base-patch16-224')
+regr_model = LinearRegression()
 
-pipe = pipeline("neuron-prediction", model=model,
-                regression_model='house', single_trial_f=MovieSingleTrialRegressionAnalysis(), train_test_size=0.7)
+pipe = pipeline("neuron-prediction",
+                model=model,
+                regression_model=regr_model,
+                single_trial_f=MovieSingleTrialRegressionAnalysis(),
+                test_set_size=0.7)
 pipe(511498742).filter_data('my_filter').plot('my_func')
 
-print(pipe.eid_dict)
+# print(pipe.eid_dict)
