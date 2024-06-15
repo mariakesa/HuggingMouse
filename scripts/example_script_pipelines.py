62f024a4c532a004a8530274f40ccbbfacfe3b04
Maria <mariarosekesa@gmail.com>
2024-06-15 10:44:41 +0300

    notebook

--- scripts/example_script_pipelines.py
+++ scripts/example_script_pipelines.py
@@ -4,8 +4,9 @@ from HuggingMouse.pipelines.single_trial_fs import MovieSingleTrialRegressionAna
 from transformers import ViTModel, CLIPVisionModel
 from sklearn.linear_model import Ridge
 
-model = ViTModel.from_pretrained('google/vit-base-patch16-224')
-model = CLIPVisionModel.from_pretrained("openai/clip-vit-base-patch16")
+# model = ViTModel.from_pretrained('google/vit-base-patch16-224')
+model = CLIPVisionModel.from_pretrained("openai/clip-vit-base-patch32")
+
 regr_model = Ridge(10)
 
 pipe = pipeline("neural-activity-prediction",
@@ -14,6 +15,6 @@ pipe = pipeline("neural-activity-prediction",
                 single_trial_f=MovieSingleTrialRegressionAnalysis(),
                 test_set_size=0.25)
 
-# 511511001 is an experiment container ID.
+# 511511001 and 646959386 are experiment container IDs.
 pipe(511511001).dropna().scatter_movies().heatmap()
-pipe(646959386).dropna().scatter_movies().heatmap()
+# pipe(646959386).dropna().scatter_movies().heatmap()
