6a65c10c54078a8e59a0ffdd1e608848f30aa2a7
Maria Kesa <maria.kesa@gmail.com>
2026-04-21 21:00:26 +0300

    add new setup.py with dependency conflicts with allensdk resolved

--- setup.py
+++ setup.py
@@ -1,21 +1,31 @@
 from setuptools import find_packages, setup
 
-setup(name='HuggingMouse',
-      version='0.2.0',
-      description='Data analysis library for Allen Brain Observatory data',
-      author='Maria Kesa',
-      author_email='mariarosekesa@gmail.com',
-      url='https://github.com/mariakesa/HuggingMouse',
-      install_requires=[
-          'scikit-learn==1.2.2',
-          'torch==1.13.1',
-          'pandas==1.5.3',
-          'numpy==1.23.5',
-          'transformers==4.31.0',
-          'allensdk==2.16.2',
-          'plotly==5.9.0',
-          #'smolagents==1.7.0'
-      ],
-      packages=find_packages("src"),
-      package_dir={"": "src"}
-      )
+setup(
+    name="HuggingMouse",
+    version="0.3.1",
+    description="Data analysis library for Allen Brain Observatory data",
+    author="Maria Kesa",
+    author_email="mariarosekesa@gmail.com",
+    url="https://github.com/mariakesa/HuggingMouse",
+    install_requires=[
+        "allensdk @ git+https://github.com/AllenInstitute/AllenSDK.git",
+        "scikit-learn>=1.2",
+        "torch>=2.1",
+        "pandas>=2.2,<3",
+        "numpy>=1.26,<3",
+        "transformers>=4.40,<5",
+        "plotly>=5.9",
+    ],
+    extras_require={
+        "agents": [
+            "smolagents>=1.7.0",
+        ],
+        "dev": [
+            "pytest>=8",
+            "black>=24",
+            "ruff>=0.5",
+        ],
+    },
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+)
